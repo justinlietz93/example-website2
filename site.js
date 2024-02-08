1581404f57079191acaf8ef4a10e87526266c63e
Justin Lietz <jlietz93@gmail.com>
2024-02-07 20:49:21 -0600

    added dynamic createCard function to display cardinfo to cards

--- site.js
+++ site.js
@@ -2,7 +2,7 @@
 
 const cardInfo1 = {
     "title": "Nike",
-	"text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     "image": "https://images.pexels.com/photos/2529148/pexels-photo-2529148.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     "color": "#deaf01"
 }
@@ -18,5 +18,34 @@ const cardInfo3 = {
     "title": "Michael Kors",
 	"text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     "image": "https://images.pexels.com/photos/2783873/pexels-photo-2783873.jpeg",
-    "color": "#01322f"
+    "color": "#01322f",
 }
+
+const title = "Adidas"
+const text = "Lorem ipsum dolor sit amet,  consectetur adipiscing elit."
+const image = "https://images.pexels.com/photos/2529148/pexels-photo-2529148.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+const color = "#deaf01"
+
+const cardInfo4 = {
+    title,
+    text,
+    image,
+    color
+}
+
+function createCard(card, info) {
+
+    card.style.backgroundImage = `url(${info.image})`
+    card.style.color = info.color
+
+    const h1 = card.querySelector('h1')
+    h1.textContent = info.title
+
+    const p = card.querySelector('p')
+    p.textContent = info.text
+}
+
+const cards = document.querySelectorAll('.card')
+createCard(cards[0], cardInfo1)
+createCard(cards[1], cardInfo2)
+createCard(cards[2], cardInfo3)
